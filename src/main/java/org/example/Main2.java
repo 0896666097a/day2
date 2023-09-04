5da731890152be9dbe40aeba38cdd4e3090556f4
PhuocDung <lephuocdung.build@gmail.com>
2023-09-04 22:10:39 +0700

    Commit

--- src/main/java/org/example/Main2.java
+++ src/main/java/org/example/Main2.java
@@ -11,8 +11,8 @@ public class Main2 {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("annotationbaseconfiguration/beans.xml");
         AccountService accountService = applicationContext.getBean("accountService", AccountService.class);
         System.out.println("Before money transfer");
-        System.out.println("Account 1 balance: "+ accountService.getAccount(1).getBalance());
-        System.out.println("Account 2 balance: "+ accountService.getAccount(2).getBalance());
+        System.out.println("Account 1: "+ accountService.getAccount(1).getBalance());
+        System.out.println("Account 2: "+ accountService.getAccount(2).getBalance());
 
         accountService.transferMoney(1,2,5.0);
         System.out.println("After money transfer");
--- src/main/java/org/example/Main3.java
+++ src/main/java/org/example/Main3.java
@@ -10,8 +10,8 @@ public class Main3 {
         AccountService accountService = applicationContext.getBean("accountService", AccountService.class);
 
         System.out.println("Before money transfer");
-        System.out.println("Account 1 balance :" + accountService.getAccount(1).getBalance());
-        System.out.println("Account 2 balance :" + accountService.getAccount(2).getBalance());
+        System.out.println("ABC 1 balance :" + accountService.getAccount(1).getBalance());
+        System.out.println("BCD 2 balance :" + accountService.getAccount(2).getBalance());
 
         accountService.transferMoney(1,2,5.0);
         System.out.println("================");
